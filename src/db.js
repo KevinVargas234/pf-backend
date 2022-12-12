60f20386ab4d3e75e290cd0b61e7cc0161341482
kevin alexis vargas parra <vargasparrakevin@gmail.com>
2022-12-11 23:30:58 -0500

    deploy

--- src/db.js
+++ src/db.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const {DB_LINK DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_USER}`,
+  DB_LINK||`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_USER}`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
