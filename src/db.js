abc60ef0d8b96fc99f0f3d6f5b3c11fec0d636b2
kevin alexis vargas parra <vargasparrakevin@gmail.com>
2022-12-11 23:32:49 -0500

    deploy

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const {DB_LINK DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const {DB_LINK,DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 const sequelize = new Sequelize(
   DB_LINK||`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_USER}`,
