bef5051a5f12b31cdd42cb63eba9df18b8a275b3
DanielGonzalez <89824250+Danielggs@users.noreply.github.com>
2022-12-15 13:14:11 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
- const adminMiddleware = require("./admin");
+
  const userMiddleware = require("./user");
 const productMiddleware = require ('./product')
 const mercadoPagoMiddleware = require ('./MercadoPago')
@@ -10,7 +10,7 @@ const router = Router();
 router.use(cors())
 router.use("/",guard);
 router.use("/mc", mercadoPagoMiddleware);
-router.use("/admin", adminMiddleware);
+
 router.use("/mc", mercadoPagoMiddleware);
 router.use("/user", userMiddleware);
 router.use('/product', productMiddleware)
